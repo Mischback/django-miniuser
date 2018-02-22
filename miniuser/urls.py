522cf992dfe7fe189087d57144ff4bdcde971bac
Mischback <dev@mischback.de>
2018-02-22 08:37:23 +0100

    Added compatibility with Django 1.10

--- miniuser/urls.py
+++ miniuser/urls.py
@@ -9,11 +9,33 @@ into your project's urls.py."""
 
 # Django imports
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView, LogoutView
+try:
+    # Django > 1.10
+    from django.contrib.auth.views import LoginView, LogoutView
+
+    def login_view():
+        """Returns an url-statement using class-based views"""
+        return url(r'^login/$', LoginView.as_view(template_name='miniuser/login.html'), name='login')
+
+    def logout_view():
+        """Returns an url-statement using class-based views"""
+        return url(r'^logout/$', LogoutView.as_view(template_name='miniuser/logout.html'), name='logout')
+
+except ImportError:
+    # Django <= 1.10
+    from django.contrib.auth.views import login, logout
+
+    def login_view():
+        """Returns an url-statement using function-based views"""
+        return url(r'^login/$', login, {'template_name': 'miniuser/login.html'}, name='login')
+
+    def logout_view():
+        """Returns an url-statement using function-based views"""
+        return url(r'^logout/$', logout, {'template_name': 'miniuser/logout.html'}, name='logout')
 
 
 app_name = 'miniuser'
 urlpatterns = [
-    url(r'^login/$', LoginView.as_view(template_name='miniuser/login.html'), name='login'),
-    url(r'^logout/$', LogoutView.as_view(template_name='miniuser/logout.html'), name='logout'),
+    login_view(),
+    logout_view(),
 ]
