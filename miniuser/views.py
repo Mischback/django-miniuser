bc1f34bcf27d609ca9228ea57e2d0da1803e9235
Mischback <dev@mischback.de>
2018-03-19 21:12:43 +0100

    Added documentation for views.py

--- miniuser/views.py
+++ miniuser/views.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-"""django-miniuser: views"""
+"""django-miniuser: views
+
+Includes all app-specific views. Most of the app's views will be re-used Django
+views, so this file only contains the absolute minimum.
+
+Logical fragments will be in the respective form classes or the app's main
+model."""
 
 # Django imports
 from django.urls import reverse_lazy
@@ -10,7 +16,7 @@ from miniuser.forms import MiniUserSignUpForm
 
 
 class MiniUserSignUpView(CreateView):
-    """foobar"""
+    """This class based view handles the registration of new users."""
 
     form_class = MiniUserSignUpForm
     success_url = reverse_lazy('miniuser:login')
